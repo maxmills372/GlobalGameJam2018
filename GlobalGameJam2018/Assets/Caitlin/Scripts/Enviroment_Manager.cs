7af0392736f16c98fbe8e65cc63ea63a00f76498
DoubleC97 <caitlin_rosser@yahoo.co.uk>
2018-01-26 23:44:32 +0000

    Setting Up My Scene

---
+++ GlobalGameJam2018/Assets/Caitlin/Scripts/Enviroment_Manager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enviroment_Manager : MonoBehaviour {
+
+    public GameObject BlueZom;
+    public GameObject RedZomb;
+    public GameObject YellowZomb;
+    public GameObject GreyZomb;
+    public GameObject Crater;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    // Checking Zombs have collided
+    // Checking what Zombs collide
+    // Checking how many zombs collide
+    void OnTriggerEnter()
+    {
+
+    }
+}
