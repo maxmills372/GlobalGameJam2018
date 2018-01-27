0aa7152610989022e9d00e5c6e54e1025a021fbb
DoubleC97 <caitlin_rosser@yahoo.co.uk>
2018-01-27 14:19:47 +0000

    Explosion done
    maybe rove some partice effects for  actual game because theres loads

---
+++ GlobalGameJam2018/Assets/Caitlin/Scripts/PlayParticle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayParticle : MonoBehaviour {
+
+	public ParticleSystem[] explode;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// Get compenent of explosion effect
+		explode = GetComponentsInChildren<ParticleSystem>();
+
+	}
+	
+	void FixedUpdate()
+	{
+		// Check if 'a' is pressed
+		if (Input.GetKeyDown ("a")) 
+		{
+			
+
+			for (int i = 0; i < explode.Length; i++) 
+			{
+				explode [i].Play();
+			}
+		}
+
+	}
+
+}
