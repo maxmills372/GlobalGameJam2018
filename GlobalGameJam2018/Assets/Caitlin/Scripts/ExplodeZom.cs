50ccd0f411633ebf8f8cafac81e07f068f2db764
DoubleC97 <caitlin_rosser@yahoo.co.uk>
2018-01-27 13:15:15 +0000

    explodes
    zoms and wall

---
+++ GlobalGameJam2018/Assets/Caitlin/Scripts/ExplodeZom.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeZom : MonoBehaviour {
+
+	public Rigidbody rb;
+	public GameObject redZom;
+
+	private float radius = 5.0f;
+	private float power = 10.0f;
+	public Vector3 explosionPos;
+
+	// Use this for initialization
+	void Start() 
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.isKinematic = true;
+
+		explosionPos = redZom.transform.position;
+
+
+
+	}
+	
+	void FixedUpdate()
+	{
+		if (Input.GetKeyDown ("a")) 
+		{
+			rb.isKinematic = false;
+			rb.AddExplosionForce (power, explosionPos, radius, 0.0f, ForceMode.Impulse);
+
+		}
+			
+	}
+}
