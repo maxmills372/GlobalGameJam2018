d44c9f5e06ed752e66ed65721d8caf3743717c79
Evilkookey <evilkookey64@gmail.com>
2018-01-27 00:58:36 +0000

    change colour on collision

--- GlobalGameJam2018/Assets/Z0MZ.cs
+++ GlobalGameJam2018/Assets/Z0MZ.cs
@@ -15,12 +15,36 @@ public class Z0MZ : MonoBehaviour {
 
 	Zom_Colour zom_colour;	// The colour that the zom is
 
+	public float speed;	// The speed of the Z0M
+
 	// Use this for initialization
 	void Start () 
 	{
-		// Assign random colour
-		zom_colour = (Zom_Colour)Random.Range(0, 4);
+		// A random number
+		int rando = Random.Range(0, 10);
 
+		// Assign random colour
+		if (rando < 7)
+		{
+			zom_colour = Zom_Colour.GREY;
+		}
+		else if (rando == 7)
+		{
+			zom_colour = Zom_Colour.RED;
+		}
+		else if (rando == 8)
+		{
+			zom_colour = Zom_Colour.BLUE;
+		}
+		else if (rando == 9)
+		{
+			zom_colour = Zom_Colour.YELLOW;
+		}
+		else
+		{
+			print("Fuck");
+		}
+			
 		// Find the renderer
 		Renderer this_renderer = gameObject.GetComponent<Renderer>();
 
@@ -48,6 +72,48 @@ public class Z0MZ : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-			
+		//find direction to centre
+		Vector3 direction = (gameObject.transform.parent.position - gameObject.transform.position);
+
+		// Move towards centre
+		transform.Translate(Vector3.Normalize(direction) * speed);
+	}
+
+	// Change colour when colliding
+	void OnCollisionEnter(Collision collision)
+	{
+		// Check if this zom is grey
+		if (zom_colour == Zom_Colour.GREY)
+		{
+			// Check if other zom is not grey
+			if(collision.collider.gameObject.GetComponent<Z0MZ>().zom_colour != Zom_Colour.GREY)
+			{
+				//Set colour to the colour of the other zom
+				zom_colour = collision.collider.gameObject.GetComponent<Z0MZ>().zom_colour;
+
+				// Find the renderer
+				Renderer this_renderer = gameObject.GetComponent<Renderer>();
+
+				// Change colour to object colour
+				switch (zom_colour)
+				{
+				case Zom_Colour.GREY:
+					this_renderer.material.color = Color.grey;
+					break;
+				case Zom_Colour.RED:
+					this_renderer.material.color = Color.red;
+					break;
+				case Zom_Colour.BLUE:
+					this_renderer.material.color = Color.blue;
+					break;
+				case Zom_Colour.YELLOW:
+					this_renderer.material.color = Color.yellow;
+					break;
+				default:
+					this_renderer.material.color = Color.black;
+					break;
+				}
+			}
+		}
 	}
 }
