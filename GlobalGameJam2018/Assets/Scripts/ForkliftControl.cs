dc808bce743f3bf896e0a2beee7d9c61bd2ed89c
maxmills372 <topmanmax@gmail.com>
2018-01-28 11:47:31 +0000

    Made forklift script

---
+++ GlobalGameJam2018/Assets/Scripts/ForkliftControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForkliftControl : MonoBehaviour {
+
+    bool move;
+    public int button_ID;
+
+    public GameObject forklift;
+    public Transform forklift_pos;
+
+    // Use this for initialization
+    void Start () {
+        move = false;
+   
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        //Forklift
+        if(move && forklift.transform.position.y >= forklift_pos.position.y)
+        {
+            forklift.transform.Translate(new Vector3(0, 0.1f, 0));
+        }
+	}
+    void OnCollisionEnter(Collision col)
+    {
+
+        if(col.collider.tag == "Player")
+        {
+
+            move = true;
+        }
+    }
+}
